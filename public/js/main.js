9a2751785007029d3b18b71b6c3e507d736c0af9
yu-ne7946 <carpdm7946@naver.com>
2018-12-29 01:15:57 +0900

    12121212

--- public/js/main.js
+++ public/js/main.js
@@ -23,6 +23,21 @@ $('.full-banner').vegas({
 });
 
 /**music control */
-$("#audio_control").click(function(){
-    Audio.pause();
-})
+// $("#audio_control").click(function(){
+//     Audio.pause();
+// })
+
+/**scroll */
+$(window).scroll(function(){
+    var gap = $(window).scrollTop();
+    if(gap > 150){
+        if(!$(".header").hasClass("gnb_color")){
+            $(".header").css({"top":"-60px"}).addClass("gnb_color");
+            $(".header").stop().animate({"top":"0px"},500);
+        }
+    }
+    else{
+        $(".header").css({"top":"-60px"}).removeClass("gnb_color");
+        $(".header").stop().animate({"top":"0px"},500);
+    }
+});

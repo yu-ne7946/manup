844dea595dacea38fc9ec3ccbe0176bf773d19d3
yu-ne7946 <carpdm7946@naver.com>
2019-01-03 10:52:23 +0900

    13

--- public/js/main.js
+++ public/js/main.js
@@ -41,3 +41,72 @@ $(window).scroll(function(){
         $(".header").stop().animate({"top":"0px"},500);
     }
 });
+
+/**banner text */
+ var interval;
+ var n = 0;
+var depth=3;
+//  $("#pro > h2").eq(0).css({"z-index":2});
+  banFade();
+  interval = setInterval(banFade,5000);
+
+//   function sleep(t){
+//       return new Promise(resolve => setTimeout(resolve,t));
+//   }
+
+ function banFade(){
+   $("#pro > h2").eq(n).animate({"opacity":1,"top": 0},1500).delay(2000)
+
+   $("#pro > h2").eq(n).animate({"opacity":0,"top": -50},1500);
+    if(n==6) {  n=-1; 
+        $("#pro >h2").animate({"top":50},100);
+   }
+     n++;
+
+};
+
+
+
+/***********music ************/
+window.onload = function () {
+ $("#play").click();
+}
+// var x = document.getElementById("audio");
+// function audioLoop() { 
+// x.loop = true;
+// x.load();} 
+
+function play() {
+    var audio = document.getElementById('audio');
+    // audio.loop = true;
+    if (audio.paused) {
+        audio.play();
+        
+        $("#play").css("color","#d91023");
+        
+        // $('#play').removeClass('glyphicon-play-circle')
+        // $('#play').addClass('glyphicon-pause')
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+        $("#play").css("color","#fff");
+        // $('#play').addClass('glyphicon-play-circle')
+        // $('#play').removeClass('glyphicon-pause')
+    }
+}
+
+/******banner scroll ************/
+var pages =[]
+var now = 0;
+$(window).resize(function(){
+    $(".page").each(function(i){
+        pages[i] =$(this).position().top;
+    });
+    // console.log(pages[1],pages[2],pages[3],pages[4],pages[5]);
+}).trigger("resize");
+
+$(".gnb").children("li").click(function(){
+    now = $(this).index();
+    console.log(pages[now]);
+    $("html").stop().animate({"scrolltop":pages[now]+"px"},300);
+}); 
